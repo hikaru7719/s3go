82c3f3910112cf99c14cc4e500a8f55ffc57064a
hikaru7719 <j148016s@st.u-gakugei.ac.jp>
2019-08-08 01:21:32 +0900

    put method

--- uploader/s3upload.go
+++ uploader/s3upload.go
@@ -1,19 +1,27 @@
 package uploader
 
 import (
+	"bytes"
+	"crypto/sha256"
+	"encoding/hex"
+	"encoding/xml"
 	"fmt"
+	"io"
 	"io/ioutil"
 	"net/http"
+	"strconv"
+	"strings"
 
 	"github.com/hikaru7719/s3go/time"
 )
 
 var (
-	BASE_HOST = "s3.amazonaws.com"
+	baseHost = "s3.amazonaws.com"
 )
 
+// New returns S3Upload
 func New(bucketName, fileName string, signature Signature) (*S3Upload, error) {
-	host := fmt.Sprintf("%s.%s", bucketName, BASE_HOST)
+	host := fmt.Sprintf("%s.%s", bucketName, baseHost)
 	return &S3Upload{
 		host:       host,
 		bucketName: bucketName,
@@ -22,25 +30,29 @@ func New(bucketName, fileName string, signature Signature) (*S3Upload, error) {
 	}, nil
 }
 
+// Signature is interface
 type Signature interface {
 	Authorization(method, URL, payload string, header map[string]string) string
 }
 
+// S3Upload is struct for upliading file to AWS S3
 type S3Upload struct {
 	host       string
 	bucketName string
 	objectName string
-	uploadId   string
+	uploadID   string
 	signature  Signature
+	file       io.Reader
 }
 
+// InitialMultipartUpload is first request to do maltipart upload
 func (s *S3Upload) InitialMultipartUpload() error {
 	client := &http.Client{}
 	req, err := s.newInitialRequest()
 	res, err := client.Do(req)
 	byteBody, err := ioutil.ReadAll(res.Body)
 	defer res.Body.Close()
-	fmt.Println(res.StatusCode, string(byteBody))
+	s.xmlMapping(byteBody)
 	return err
 }
 
@@ -56,10 +68,55 @@ func (s *S3Upload) newInitialRequest() (*http.Request, error) {
 	return req, err
 }
 
+type initialRespXML struct {
+	UploadID string `xml:"UploadId"`
+}
+
+func (s *S3Upload) xmlMapping(respBody []byte) {
+	xmlMapper := initialRespXML{}
+	xml.Unmarshal(respBody, &xmlMapper)
+	s.uploadID = xmlMapper.UploadID
+}
+
 func (s *S3Upload) convertToMap(header http.Header) map[string]string {
 	newMap := make(map[string]string)
 	for key := range header {
 		newMap[key] = header.Get(key)
 	}
 	return newMap
+}
+
+// PutMaltiPartObject is request to upload object
+func (s *S3Upload) PutMaltiPartObject() error {
+	client := &http.Client{}
+	req, err := s.newUploaderRequest()
+	res, err := client.Do(req)
+	if err != nil {
+		return err
+	}
+	defer res.Body.Close()
+	byteBody, err := ioutil.ReadAll(res.Body)
+	fmt.Println(res.StatusCode, string(byteBody))
+	return err
+}
+
+func (s *S3Upload) newUploaderRequest() (*http.Request, error) {
+	url := fmt.Sprintf("https://%s/%s?partNumber=1&uploadId=%s", s.host, s.objectName, s.uploadID)
+	byteBody, _ := ioutil.ReadAll(s.file)
+	buffer := bytes.NewBuffer(byteBody)
+	req, err := http.NewRequest("PUT", url, buffer)
+	req.Header.Add("x-amz-date", time.Default.Now())
+	req.Header.Add("Host", s.host)
+	req.Header.Add("x-amz-content-sha256", hashSHA256(string(byteBody)))
+	req.Header.Add("Content-Length", strconv.Itoa(len(byteBody)))
+	headerMap := s.convertToMap(req.Header)
+	authorization := s.signature.Authorization("PUT", url, string(byteBody), headerMap)
+	req.Header.Add("Authorization", authorization)
+	return req, err
+}
+
+func hashSHA256(payload string) string {
+	hash := sha256.Sum256([]byte(payload))
+	hexed := hex.EncodeToString(hash[:])
+	return strings.ToLower(hexed)
 }
--- uploader/s3upload_test.go
+++ uploader/s3upload_test.go
@@ -1,13 +1,66 @@
 package uploader
 
 import (
+	"os"
 	"testing"
 
 	"github.com/hikaru7719/s3go/signature"
+	"github.com/stretchr/testify/assert"
 )
 
 func TestInitialMultipartUpload(t *testing.T) {
 	sig := signature.New()
 	upload, _ := New("s3go-cli-test", "test", sig)
 	upload.InitialMultipartUpload()
+}
+
+func TestNewInitialRequest(t *testing.T) {
+
+	uploader := &S3Upload{
+		host:       "testhost",
+		bucketName: "testbucket",
+		objectName: "testObject",
+		signature:  &mockAuth{},
+	}
+
+	req, _ := uploader.newInitialRequest()
+	assert.Equal(t, "testhost", req.Header.Get("Host"))
+	assert.Equal(t, "e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855", req.Header.Get("x-amz-content-sha256"))
+	assert.Equal(t, "testAuthorization", req.Header.Get("Authorization"))
+
+}
+
+func TestXMLMapping(t *testing.T) {
+	uploader := &S3Upload{}
+	testXML := `<?xml version="1.0" encoding="UTF-8"?><InitiateMultipartUploadResult xmlns="http://s3.amazonaws.com/doc/2006-03-01/"><Bucket>test</Bucket><Key>test</Key><UploadId>hogehoge</UploadId></InitiateMultipartUploadResult>`
+	uploader.xmlMapping([]byte(testXML))
+	assert.Equal(t, "hogehoge", uploader.uploadID)
+}
+
+type mockAuth struct{}
+
+func (m *mockAuth) Authorization(method, URL, payload string, header map[string]string) string {
+	return "testAuthorization"
+}
+
+func TestPutMultipartObject(t *testing.T) {
+	sig := signature.New()
+	upload, _ := New("s3go-cli-test", "test", sig)
+	file, _ := os.Open("earth.jpg")
+	upload.file = file
+	upload.InitialMultipartUpload()
+	upload.PutMaltiPartObject()
+}
+
+func TestNewUploadRequest(t *testing.T) {
+	upload := &S3Upload{
+		host:       "testhost",
+		bucketName: "testbucket",
+		objectName: "testObject",
+		signature:  &mockAuth{},
+	}
+	file, _ := os.Open("earth.jpg")
+	upload.file = file
+	req, _ := upload.newUploaderRequest()
+	assert.Equal(t, "hoge", req.Header.Get("content-length"))
 }
